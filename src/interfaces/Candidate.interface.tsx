7991c920b4005410d80d271c3da8a7b025e031ef
kagreene <kristengreene2023@U.northwestern.edu>
2025-01-08 10:27:59 -0600

    added candidate interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,10 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export default interface Candidate {
+    readonly name: string;
+    readonly username: string;
+    readonly location: string;
+    readonly avatar: string;
+    readonly email: string;
+    readonly html_url: string;
+    readonly company: string;
+}
