020bc23bc811e178b2495e81addf3ea1560c1bb3
kagreene <kristengreene2023@U.northwestern.edu>
2025-01-10 10:43:22 -0600

    completed search file

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
+import { FormEvent } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import CandidateCard from '../components/CandidateCard';
 import type Candidate from '../interfaces/Candidate.interface';
 
 
@@ -24,12 +26,40 @@ const CandidateSearch = () => {
     parsedSavedCandidates.push(currentCandidate);
     localStorage.setItem('savedCandidates', JSON.stringify(parsedSavedCandidates));
   };
+ //load a random candidate when the page loads
+  useEffect(() => {
+    randomSearch();
+  }, []);
+  
+  //Rejecting candidate should just be setting the current candidate to a new random candidate
+const rejectCandidate = async () => {
+  await randomSearch();
+}
 
-  //add code for rejecting a candidate
-
+  //code to search github for a random user
+  const randomSearch = async () => {
+    const randomUser = await searchGithub();
+    setCurrentCandidate(randomUser);
+  };
+  // code to search for candidate by username
+  const searchByUser = async (event: FormEvent, user: string) => {
+    event.preventDefault();
+    const userSearch = await searchGithubUser(user);
+    setCurrentCandidate(userSearch);
+  }
+  //When are we supposed to use the search by candidate username function?
+  //The readme does not mention searching by username
   
 
-  return <h1>CandidateSearch</h1>;
+  return 
+  <>
+  <h1>CandidateSearch</h1>;
+  <CandidateCard
+  currentCandidate={currentCandidate}
+  saveCandidate={saveCandidate}
+  removeCandidate={rejectCandidate}/>
+  </>
 };
 
 export default CandidateSearch;
+
