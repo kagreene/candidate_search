8cbf187e2616440798159f290c5e4f9c1c964be5
kagreene <kristengreene2023@U.northwestern.edu>
2025-01-14 11:47:12 -0600

    fixed display issues

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -2,9 +2,10 @@
 export default interface Candidate {
     readonly name: string;
     readonly username: string;
-    readonly location: string;
-    readonly avatar: string;
-    readonly email: string;
+    readonly location?: string;
+    readonly avatar_url?: string;
+    readonly email?: string;
     readonly html_url: string;
-    readonly company: string;
+    readonly company?: string;
+    readonly bio?: string;
 }
